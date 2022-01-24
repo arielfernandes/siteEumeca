cb4ada0d38b4376c58babac425c8189331580fa5
arielfernandes3 <0.arielfernandes.0@gmail.com>
2022-01-24 10:38:07 -0300

    config server heroku

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template, Blueprint
 from src.eumeca_blueprint.eumeca import eumeca_blueprint
-
 app = Flask(__name__)
-
 app.register_blueprint(eumeca_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
